f90509d74d61e1ec30413e3d0ff59983c8534fa6
cliffjr <cliffordjmthemba@gmail.com>
2020-06-25 11:57:29 +0200

    Fixed bug

---
+++ build/precache-manifest.c806292e75163f21b728b1a80217a249.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7f2fd420cc6b0978a79d7b7a4d8f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d9ba16169d5971cfb7",
+    "url": "/static/css/2.7e9ee316.chunk.css"
+  },
+  {
+    "revision": "a2d57befccba1a505d2f",
+    "url": "/static/css/main.06f87dc0.chunk.css"
+  },
+  {
+    "revision": "d3d9ba16169d5971cfb7",
+    "url": "/static/js/2.fc32455d.chunk.js"
+  },
+  {
+    "revision": "a2d57befccba1a505d2f",
+    "url": "/static/js/main.0d08db68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3e95d5c98547a1914b493e2898d6743",
+    "url": "/static/media/IM2783RE.e3e95d5c.JPG"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "03c6dfef67fe4520f07ba2ce097d7e83",
+    "url": "/static/media/solidarmedlogo.03c6dfef.png"
+  }
+]);
